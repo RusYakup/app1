a8ca9753254a9416907d95a7655858bebc065e33
RusYakup <zregxbr@ya.ru>
2021-10-17 17:28:15 +0300

    edit

--- AppGolang/main.go
+++ AppGolang/main.go
@@ -1,29 +1,24 @@
 package main
 
 import (
-	_ "go/ast"
-	"html/template"
-	"log"
 	"net/http"
-)
-func main()  {
-	http.HandleFunc("/", mainPage)
 
-	port := ":9090"
-	println("server listent port", port)
-	err := http.ListenAndServe(port,nil)
-	if err != nil {
-		log.Fatal("listenand server", err)
-	}
+	"github.com/gin-gonic/gin"
+)
+
+func HelloWorld(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"title": "Hello world",
+	})
 }
-func mainPage(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("index.html")
-	if err!= nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-	if err:= tmpl.Execute(w, nil); err !=nil {
-		http.Error(w, err.Error(), 400)
-		return
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLFiles("index.html")
+	r.GET("/", HelloWorld)
+
+	err := r.Run()
+	if err != nil {
+		panic(err)
 	}
 }

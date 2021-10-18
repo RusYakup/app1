b21b0cead6158dcee61cb310b60bfc0ee03c1739
RusYakup <zregxbr@ya.ru>
2021-10-18 21:41:26 +0300

    edit

--- main.go
+++ main.go
@@ -1,39 +1,24 @@
 package main
 
 import (
-	_ "go/ast"
-	"html/template"
-	"log"
+	"fmt"
 	"net/http"
-	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
-
-func main()  {
-	http.HandleFunc("/", HelloWorldHandler)
-
-	port := ":9090"
-	println("server listen port", port)
-	err := http.ListenAndServe(port,nil)
-	if err != nil {
-		log.Fatal("App failed with error:", err)
-	}
+func HelloWorld(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{"title": "Exam andersen"})
 }
-func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	path, err := os.Getwd()
-	if err != nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-	filepath := path + "/index.html"
 
-	tmpl, err := template.ParseFiles(filepath)
-	if err!= nil {
-		http.Error(w, err.Error(), 400)
-		return
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("index.html")
+
+	router.GET("/", HelloWorld)
+	err := router.Run(":80")
+	if err != nil {
+		fmt.Print("all ok")
 	}
-	if err:= tmpl.Execute(w, nil); err !=nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
+
 }
